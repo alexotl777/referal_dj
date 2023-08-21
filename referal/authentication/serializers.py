5c033cccae6df7b97e3dac7b5f5ade830903d364
alexotl777 <alexotl228@yandex.ru>
2023-08-21 13:21:58 +0300

    add models, view for phone with template and .css

---
+++ referal/authentication/serializers.py
@@ -0,0 +1,9 @@
+#from rest_framework import serializers
+from itsdangerous import Serializer
+from .models import authentication
+ 
+ 
+class WomenSerializer(Serializer.ModelSerializer):
+    class Meta:
+        model = authentication
+        fields = ('title')
---
+++ referal/authentication/views.py
@@ -0,0 +1,14 @@
+from django.forms import GenericIPAddressField
+from django.shortcuts import render
+#from rest_framework import generics
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponsePermanentRedirect, JsonResponse
+from django.views.generic import TemplateView
+from authentication.models import authentication
+# Create your views here.
+
+class authAPI(TemplateView):
+    template_name = 'referal.html'
+    queryset = authentication.objects.all()
+    def form_phone(self, request):
+        ctx = {}
+        return render(request, self.template_name, ctx)
